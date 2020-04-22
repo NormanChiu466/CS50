809cd4ddfed5a389c9215d5cc73f63c9cee32bfc
NormanChiu466 <64171891+NormanChiu466@users.noreply.github.com>
2020-04-22 18:35:45 -0400

    Create Agree.C

---
+++ Agree.C
@@ -0,0 +1,18 @@
+// Logical Operators
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main (void)
+{
+    // Prompt user to agree
+    char c = get_char ("Do you agree?\n");
+    
+    // Check whether agreed
+    if (c == 'Y' || c == 'y'
+     {
+          printf ("Agree.\n"}l
+     {
+     else if (c == 'N' || c == 'n')
+     {
+          printf ("
